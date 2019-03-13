acd10efe71b8a73df450dabae0ec6e2238664c7f
Sachin Saini <iamsainisachin@gmail.com>
2019-03-13 23:26:49 +0530

    add CORS middleware

--- app/index.js
+++ app/index.js
@@ -3,7 +3,14 @@ const app = express();
 const channelRoutes = require('./channel/routes');
 
 app.use(express.json());
+const cors = (req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+	next();
+};
+app.use(cors);
 app.use('/', channelRoutes);
 
 module.exports = app;
