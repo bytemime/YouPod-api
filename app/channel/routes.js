e9b9725893b24ae3cd07d6acdd33f3e5d176c9d0
Sachin Saini <iamsainisachin@gmail.com>
2019-03-13 23:25:54 +0530

    add latest video route

--- app/channel/routes.js
+++ app/channel/routes.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+// import config
+const config = require('../config');
+// declare constants
+const BASE_URL = 'https://www.googleapis.com/youtube/v3';
+const API_KEY = config.youtubeAPI.key;
+const ACTION = 'search';
 
-router.get('/', (req, res) => {
-	res.status(200).json('access not allowed');
-});
+// fetch latest videos depending upon the number of videos
+// requested by the client
+async function getLatestVideos(req, res) {
+	const URL = `${BASE_URL}/${ACTION}?part=snippet&channelId=${
+		req.body.channelId
+	}&maxResults=${req.body.maxResults}&order=date&type=video&key=${API_KEY}`;
+	let response = await axios.get(URL);
+	// map over the response and select only the needed parts
+	// and create the response to be sent
+	let responseArray = response.data.items.map(i => {
+		return {
+			title: i.snippet.title,
+			publishDate: i.snippet.publishedAt,
+			channelId: i.snippet.channelId,
+			videoId: i.id.videoId,
+			description: i.snippet.description
+		};
+	});
+	res.json(responseArray);
+}
+// latest video route
+router.post('/api/get-channel-details', getLatestVideos);
 module.exports = router;
